aa156fbccd699ac42d732bdcacb5b0aa9729f4c4
C0ntrolFr3ak <rob007.thomson@gmail.com>
2024-08-12 12:32:39 +1200

    answer model added

--- polls/models.py
+++ polls/models.py
@@ -14,3 +14,11 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)
+
+    def __str__(self):
+       return self.answer_text
